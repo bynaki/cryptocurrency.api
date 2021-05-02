bc3753f1997678a75325f062f29b8aff114597de
naki <bynaki@icloud.com>
2021-05-02 16:26:20 +0900

    fixed modules name

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export {AxiosError} from 'axios'
+export {
+  UPbit,
+} from './upbit'
+export * as upbit_types from './upbit.types'
+export {
+  Binance,
+} from './binance'
+export * as binance_types from './binance.types'
